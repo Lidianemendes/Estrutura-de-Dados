16c14838d99b577232f20d39f6f25addf6e0db03
lidianemendes <lidianemendes98@yahoo.com.br>
2022-01-13 00:52:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = "Lidiane"
+let number = 78
+let anoAtual = 2022
+let year = anoAtual - number
+console.log(number + year)
+
+console.log("Estou mexendo no console.log")
+
+let day = "quartaFeira"
+
+console.log(day == "quartaFeira")
+
+console.log(typeof true)
